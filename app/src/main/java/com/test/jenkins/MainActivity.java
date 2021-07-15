dd2dba719f9c55806ac5d5d232a2f8f71a47a9b8
chengjie <chengjie0521@ubox.cn>
2021-07-15 14:55:48 +0800

    sign

--- app/src/main/java/com/test/jenkins/MainActivity.java
+++ app/src/main/java/com/test/jenkins/MainActivity.java
@@ -3,12 +3,19 @@ package com.test.jenkins;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTv_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTv_show = findViewById(R.id.tv_show);
+        mTv_show.setText(String.format("BASEURL=%s,\r\n commonId=%s",BuildConfig.BASEURL,BuildConfig.commonId));
+
     }
 }
